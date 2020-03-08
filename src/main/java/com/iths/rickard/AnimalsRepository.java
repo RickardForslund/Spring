c679fcc0ca46d6e64d12c533e5b4a11a20ec471e
Rikard <Rickard.forslund@live.se>
2020-03-08 23:43:31 +0100

    last push

--- src/main/java/com/iths/rickard/AnimalsRepository.java
+++ src/main/java/com/iths/rickard/AnimalsRepository.java
@@ -4,13 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnimalsRepository extends JpaRepository<Cat, Long> {
 
-    //Custom repository methods
     Cat findByName(String name);
-
     Object deleteById(int i);
-
-//    List<Cat> findByIdGreaterThan(Long id);
-//    List<Cat> findAllByNameIsContaining(String name);
-//    @Query("Select p from Person p where lower(p.name) like lower(?1)")
-//    List<Cat> findByNameCustom(String name);
 }
